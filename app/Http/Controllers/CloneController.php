fbea0ec100fa690d03804f0e216997ac2ab2674f
IrsyadramadhaniS <133120488+IrsyadramadhaniS@users.noreply.github.com>
2023-05-10 20:23:07 +0700

    Add files via upload

---
+++ app/Http/Controllers/CloneController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CloneController extends Controller
+{
+    public function botsrap(){
+        return view("Modul1.assets.index");
+    }
+}
